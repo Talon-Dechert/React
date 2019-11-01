4facae741495b48d417542104641e6c4fd64185e
Talon-Dechert <tdechert@live.com>
2019-10-31 22:22:07 -0700

    Added react router and mobile navbar

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
   //* The <Menu dishes={this.state.dishes} /> makes the dishes state pass down into the menu component
