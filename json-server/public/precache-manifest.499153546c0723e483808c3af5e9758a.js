3c74789ab89942c2e73b1cc7b5f3902809f44f83
Talon-Dechert <tdechert@live.com>
2019-11-17 11:10:05 -0800

    Made build version ready to test out server

---
+++ json-server/public/precache-manifest.499153546c0723e483808c3af5e9758a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c6741b8113e17c8929e8f6a633663d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6af61a1fb140743bdf",
+    "url": "/static/css/2.c73ce0a8.chunk.css"
+  },
+  {
+    "revision": "aab0bcd883136afe587e",
+    "url": "/static/css/main.95a7c173.chunk.css"
+  },
+  {
+    "revision": "1e6af61a1fb140743bdf",
+    "url": "/static/js/2.3bb3bef5.chunk.js"
+  },
+  {
+    "revision": "aab0bcd883136afe587e",
+    "url": "/static/js/main.95195e83.chunk.js"
+  },
+  {
+    "revision": "8870a603714713bb2741",
+    "url": "/static/js/runtime-main.17759ccd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
